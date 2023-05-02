1cfc4d4a0830e90793f6f5441e2d8082bcd81f0f
giantolentino <gianpaolotolentino@gmail.com>
2023-05-01 21:56:12 -0700

    Updated js to work with hours and mins.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function updateTime() {
-  const clock = document.querySelector('.clock');
+  const hour = document.querySelector('.hour');
+  const mins = document.querySelector('.mins');
   const now = new Date();
   const hours = now.getHours().toString().padStart(2, '0');
   const minutes = now.getMinutes().toString().padStart(2, '0');
-  //   const seconds = now.getSeconds().toString().padStart(2, '0');
-  //   clock.textContent = `${hours}:${minutes}:${seconds}`;
-  clock.textContent = `${hours}:${minutes}`;
+  hour.textContent = hours;
+  mins.textContent = minutes;
 }
 
 updateTime();
