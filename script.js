c42694723d4abad7f15daffe8c05fc11d6e59433
giantolentino <gianpaolotolentino@gmail.com>
2023-05-01 21:21:17 -0700

    Removed seconds in clock.

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function updateTime() {
   const now = new Date();
   const hours = now.getHours().toString().padStart(2, '0');
   const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
-  clock.textContent = `${hours}:${minutes}:${seconds}`;
+  //   const seconds = now.getSeconds().toString().padStart(2, '0');
+  //   clock.textContent = `${hours}:${minutes}:${seconds}`;
+  clock.textContent = `${hours}:${minutes}`;
 }
 
 updateTime();
